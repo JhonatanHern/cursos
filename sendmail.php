e1e6e33ebf1f29952161ebec798d8d3685120fad
JhonatanHern <jhonatanhernandez998@gmail.com>
2017-12-19 16:58:31 -0400

    webpage finished

--- sendmail.php
+++ sendmail.php
@@ -2,7 +2,7 @@
 	$recaptcha = $_POST["g-recaptcha-response"];
 	$url = 'https://www.google.com/recaptcha/api/siteverify';
 	$data = array(
-		'secret' => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+		'secret' => 'XXXXXXXXXXX',
 		'response' => $recaptcha
 	);
 	$options = array(
@@ -15,6 +15,26 @@
 	$verify = file_get_contents($url, false, $context);
 	$captcha_success = json_decode($verify);
 	if ($captcha_success->success) {
+		$to      = 'sistemas.serofca@gmail.com';
+		$subject = 'contacto - cursos.serofca.com';
+		$name = $_POST['name'];
+		$email = $_POST['mail'];
+		$cel = $_POST['phone'];
+		$country = $_POST['country'];
+		$msg = $_POST['msg'];
+			
+		$message = "de: $name ($email)\n Desde $country\n$cel\n\n$msg";
+		$headers = 'From: notificaciones@serofca.com' . "\r\n" .
+			'Reply-To: notificaciones@serofca.com' . "\r\n" .
+			'X-Mailer: PHP/' . phpversion();
+		mail($to, $subject, $message, $headers);
+		echo "
+			<script>
+				alert('mensaje enviado');
+				location.href = 'https://curso.serofca.com'
+			</script>
+		";
 	} else {
+		echo 'error en autenticación. Posible bot detectado, intente de nuevo.';
 	}
 ?>
